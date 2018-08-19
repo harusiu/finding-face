eafeabdaaec91d6da8be390de4cd104fc03180eb
Haru Siu <siu910223@gmail.com>
2018-08-19 16:07:11 +0800

    fix error

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://obscure-plains-18233.herokuapp.com//signin', {
+		fetch('https://obscure-plains-18233.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
